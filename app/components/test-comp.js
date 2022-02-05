5873e949184d30c54a084b3ba43489a21cb37f6b
ericg <ericgorr@gmail.com>
2022-02-04 22:09:38 -0500

    Needed to use an older version of ember-responsive which supports Ember 3.4

--- app/components/test-comp.js
+++ app/components/test-comp.js
@@ -1,4 +1,28 @@
-import Component from '@ember/component';
+import { computed } from '@ember/object';
+import Table from 'ember-light-table';
 
-export default Component.extend({
+export default Ember.Component.extend({
+  model: [
+    { firstName: 'Henry', lastName: 'Smith' },
+    { firstName: 'Henry', lastName: 'Smith' },
+    { firstName: 'Henry', lastName: 'Smith' },
+    { firstName: 'Henry', lastName: 'Smith' },
+    { firstName: 'Henry', lastName: 'Smith' },
+    { firstName: 'Henry', lastName: 'Smith' } ],
+
+  columns: computed(function() {
+    return [{
+      label: 'First Name',
+      valuePath: 'firstName',
+      width: '150px'
+    }, {
+      label: 'Last Name',
+      valuePath: 'lastName',
+      width: '150px'
+    }];
+  }),
+
+  table: computed('model', function() {
+   return new Table(this.get('columns'), this.get('model'));
+  })
 });
